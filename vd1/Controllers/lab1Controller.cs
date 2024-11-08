c2f29aa2f443488b3e4bca2088d2ae1a13ef8d7d
thanhan <anntpd10748@gmail.com>
2024-11-08 08:59:26 +0700

    lab1

---
+++ vd1/Controllers/lab1Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vd1.Models;
+
+namespace vd1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class lab1Controller : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            Game106Model game106 = new Game106Model
+            {
+                CourseName = "Back-End Game Programming",
+                CourseCode = "gam106",
+                Name = "nguyen thanh an",
+                StudentCode = "pd10748",
+                ClassName = "GA19301"
+            };
+            int status = 1;
+            string mesaage = "Get data sucess!";
+            var data = new {status, mesaage, game106};
+            return new JsonResult(data);
+        }
+    }
+}
